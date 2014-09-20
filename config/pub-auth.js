9e735556633654d132433e1e0b5ab188d4a23744
trance-with-me <amb@sonic.net>
2014-09-20 00:06:19 -0700

    Replace auth.js file with template

---
+++ config/pub-auth.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+
+  'amazon_s3' : {
+    'SECRET_ACCESS_KEY' : 'XXX',
+    'ACCESS_KEY_ID'     : 'XXX',
+    'BUCKET_PERMANENT'  : 'numa-images',
+    'BUCKET_TEMPORARY'  : 'numa-temp'
+  },
+
+  'facebook' : {
+    'CLIENT_SECRET'     : 'XXX'
+  },
+
+  'github' : {
+    'CLIENT_SECRET'     : 'XXX'
+  },
+
+  'google' : {
+    'CLIENT_SECRET'     : 'XXX'
+  },
+
+  'TOKEN_SECRET'        : 'XXX',
+  'ENCRYPTION_KEY'      : 'XXX',
+  'ENCRYPTION_ALGO'     : 'aes128',
+  'HASHIDS_SALT'        : 'XXX'
+
+};
